d542f7d4b0c7c020f7c9f3d0e5e20c52596290a0
Lalith Vipulananthan <lalithv@gmail.com>
2021-12-03 23:57:21 +0900

    Update day3.py
    Refactoring part1 due to being baffled by part2 lol

--- 2021/day3.py
+++ 2021/day3.py
@@ -3,30 +3,27 @@ def load_input():
         return input.read().splitlines()
 
 def part1(diagnostics):
-    diagnostics_size_check = len(diagnostics) // 2
     # create list of vertical slices through the diagnostics array
     verticals = [[y[x] for y in diagnostics] for x in range(len(diagnostics[0]))]
 
-    # next loop through each element, sum the '1's and see if the total is greater than half the size of the diagnostics
-    gamma_rate = ''
-    for v in verticals:
-        if sum(x[0]=='1' for x in v) > diagnostics_size_check:
-             gamma_rate += '1'
-        else:
-            gamma_rate += '0'
-    epsilon_rate = ''.join('1' if x == '0' else '0' for x in gamma_rate)
-    gamma_rate_decimal = int(gamma_rate, 2)
-    epsilon_rate_decimal = int(epsilon_rate, 2)
+    # enumerate each element and compare the sum of '1's and '0's
+    gamma_rate, epsilon_rate = [], []
+    for i,x in enumerate(verticals):
+        bit_check = 1 if x.count('1') > x.count('0') else 0
+        gamma_rate.append(bit_check)
+        epsilon_rate.append(int(not(bit_check)))
+    gamma_rate_decimal = int(''.join([str(x) for x in gamma_rate]), 2)
+    epsilon_rate_decimal = int(''.join([str(x) for x in epsilon_rate]), 2)
 
     print(gamma_rate_decimal*epsilon_rate_decimal)  
 
 def part2(diagnostics):
     return 0
-    
+
 def main():
     diagnostics = load_input()
     part1(diagnostics)
-    #part2(diagnostics)
+    part2(diagnostics)
 
 if __name__ == "__main__":
     main()
