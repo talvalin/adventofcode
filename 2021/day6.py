df6cba5539805d0ef51600f5bb141bd096f48fdb
Lalith Vipulananthan <lalithv@gmail.com>
2021-12-08 22:24:39 +0900

    Update day6.py
    After the brute force method of part 1 failed, I went with a simple array and what do you know, it ran a whole lot faster!

--- 2021/day6.py
+++ 2021/day6.py
@@ -1,7 +1,5 @@
-import itertools
-
 def load_input():
-    with open('../aoc_inputs/2021/day06_test_input.txt') as input:
+    with open('../aoc_inputs/2021/day06_input.txt') as input:
         return input.read().splitlines()
 
 def part1(input, days):
@@ -16,13 +14,34 @@ def part1(input, days):
         input.extend([8 for i in range(new_fish)])      
     return len(input)
 
-def part2():
-    pass
+def part2(input, days):
+    # create fish school
+    fish_school = [0] * 9
+    print(len(fish_school))
+    
+    # initial update of school
+    for x in input:
+        fish_school[x] += 1
+
+    previous_day_new_fish = 0    
+    for day in range(0, days):
+        # rotate array
+        for i in range(8):
+            fish_school[i] = fish_school[i+1]
+        fish_school[8] = 0
+
+        # add fish in the zero index to the 6th index and the 8th index
+        if previous_day_new_fish:   
+            fish_school[6] += previous_day_new_fish
+            fish_school[8] = previous_day_new_fish
+        previous_day_new_fish = fish_school[0]
+
+    return sum(x for x in fish_school)
 
 def main():
     input = [int(x) for x in load_input()[0].split(',')]
     print(part1(input, 256))
+    print(part2(input, 256))
     
-
 if __name__ == "__main__":
     main()
